e3bc3f32130a1e98abe2e5d19aaffafa2bbbd56c
arix2000 <arix20003@gmail.com>
2023-12-10 16:38:57 +0100

    remove state in Tags

--- app/src/main/java/com/note/app/tutorial/features/addEditNote/ui/AddEditNoteScreen.kt
+++ app/src/main/java/com/note/app/tutorial/features/addEditNote/ui/AddEditNoteScreen.kt
@@ -1,8 +1,6 @@
 package com.note.app.tutorial.features.addEditNote.ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -10,47 +8,32 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Cancel
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.note.app.tutorial.features.notes.ui.Note
-import com.note.app.tutorial.features.notes.ui.exampleNotes
+import com.note.app.tutorial.features.notes.ui.NoteTag
 import com.note.app.tutorial.ui.theme.NoteAppTutorialTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddEditNoteScreen(noteId: Int?, navController: NavHostController) {
-    val note = exampleNotes[noteId ?: 1]
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -80,7 +63,7 @@ fun AddEditNoteScreen(noteId: Int?, navController: NavHostController) {
             modifier = Modifier.weight(1f)
         )
         Row(verticalAlignment = Alignment.CenterVertically) {
-            TagToggleButtons(note)
+            TagToggleButtons(NoteTag("none", Color.Transparent), onTagSelected = {})
             Spacer(modifier = Modifier.weight(1f))
             Button(onClick = { }) {
                 Text(text = "Zapisz")
@@ -92,17 +75,16 @@ fun AddEditNoteScreen(noteId: Int?, navController: NavHostController) {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun TagToggleButtons(note: Note?) {
-    var selected: String? by remember { mutableStateOf(null) }
+private fun TagToggleButtons(selectedTag: NoteTag, onTagSelected: (NoteTag) -> Unit) {
     Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
         FilterChip(
             onClick = {
-                selected = if (selected == "Praca") null else "Praca"
+                onTagSelected(NoteTag("Praca", Color.Blue.copy(0.3f)))
             },
             label = {
                 Text("Praca")
             },
-            selected = selected == "Praca",
+            selected = selectedTag.name == "Praca",
             shape = CircleShape,
             colors = FilterChipDefaults.filterChipColors(
                 selectedContainerColor = Color.Blue.copy(0.3f)
@@ -110,12 +92,12 @@ private fun TagToggleButtons(note: Note?) {
         )
         FilterChip(
             onClick = {
-                selected = if (selected == "Zabawa") null else "Zabawa"
+                onTagSelected(NoteTag("Zabawa", Color.Red.copy(0.3f)))
             },
             label = {
                 Text("Zabawa")
             },
-            selected = selected == "Zabawa",
+            selected = selectedTag.name == "Zabawa",
             shape = CircleShape,
             colors = FilterChipDefaults.filterChipColors(
                 selectedContainerColor = Color.Red.copy(0.3f)
@@ -123,17 +105,15 @@ private fun TagToggleButtons(note: Note?) {
         )
         FilterChip(
             onClick = {
-                selected = if (selected == "Nauka") null else "Nauka"
+                onTagSelected(NoteTag("Nauka", Color.Green.copy(0.3f)))
             },
             label = {
                 Text("Nauka")
             },
-            selected = selected == "Nauka",
+            selected = selectedTag.name == "Nauka",
             shape = CircleShape,
             colors = FilterChipDefaults.filterChipColors(
-                selectedContainerColor = Color.Green.copy(
-                    0.3f
-                )
+                selectedContainerColor = Color.Green.copy(0.3f)
             )
         )
     }
